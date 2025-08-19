ce98d8dea021a1f3064ac972f5803a8c1b5acdff
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-08-19 15:46:17 -0400

    added shadcn ui components of avatar, badge, table, skeleton, card and updated globals.css file with colors,

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header"
+import Header from "@/components/header/header"
 import type { Metadata } from "next"
 import { Space_Grotesk } from "next/font/google"
 import "./globals.css"
