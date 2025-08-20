6e24113e9d57dbcb853cb19f0822d6e5e5112535
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-08-19 20:36:08 -0400

    added lib functions

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,1 +1,40 @@
 import { AnalyticsInfoResponse, JourneyInsightsResponse } from "@/@types/analytics"
+import { DateRange} from "@/@types/common"
+import type { Analytics as PrismaAnalytics, VisitorFilter } from '@/prisma/app/generated/prisma'
+import { slow } from '@/utils/common'
+import { prisma } from "./prisma"
+
+export const getAnalytics = async (): Promise<PrismaAnalytics[]> => {
+    await slow(8000)
+    const currentAnalytics = await prisma.analytics.findMany({
+        orderBy: { date: "asc" },
+        take: 7
+    })
+
+    return currentAnalytics
+}
+
+export const getJourneyInsights = 
+    async (): Promise<JourneyInsightsResponse> => {
+        await slow(3000)
+        const journeys = await prisma.visitorJourney.findMany({
+            orderBy: { completedAt: "desc" },
+            take: 10
+        })
+
+        return journeys
+    }
+
+    export const getVisitorFilters = async (): Promise<VisitorFilter[]> => {
+        await slow(1000)
+        return await prisma.visitorFilter.findMany()
+    }
+
+    export const getAnalyticsInfo = async (): Promise<AnalyticsInfoResponse> => {
+        await slow(1000)
+        return await prisma.analyticsInfo.findUnique({
+            where: {
+                domain: "nextjscourse.dev"
+            }
+        })
+    }
---
+++ lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@/prisma/app/generated/prisma/client"
+
+import { withAccelerate } from "@/prisma/extension-accelerate"
+
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
+}
+
+export const prisma = new PrismaClient().$extends(withAccelerate())
+
