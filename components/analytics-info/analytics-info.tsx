83cea58da53856ba51adc6b44859ac43ecd47ce7
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-08-19 20:49:01 -0400

    added analytics info component

--- components/analytics-info/analytics-info.tsx
+++ components/analytics-info/analytics-info.tsx
@@ -1,1 +1,32 @@
 import { Badge } from "@/components/ui/badge"
+import { getAnalyticsInfo } from "@/lib/analytics"
+import { AnalyticsInfoResponse } from "@/types/analytics"
+import { InfoItem } from "@/@types/components"
+
+export async function AnalyticsInfo() {
+    const analyticsInfo: AnalyticsInfoResponse = await getAnalyticsInfo()
+
+    if (!analyticsInfo) {
+        return <></>
+    }
+
+    const infoItems: InfoItem[] = [
+        { id: "domain", label: "Domain", value: domain },
+        { id: "createdAt", label: "Created At", value: createdAt ? new Date(createdAt).toLocaleDateString() : "" },
+    ]
+
+    return (
+        <div className="flex-gap-2">
+            { infoItems.map( item => (
+                <Badge
+                    key={item.id}
+                    variant="outline"
+                    className="text-sm font-bold text-black bg-pink-300 inline-block px-2 py-1 border-2
+                        border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)]"
+                >
+                    { item.label }: { item.value }
+                </Badge>
+            ))}
+        </div>
+    )
+}
